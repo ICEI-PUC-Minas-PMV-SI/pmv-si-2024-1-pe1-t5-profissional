3a59ef4ebe95fab4b9502a4d0f344d059e5e4edd
Luc4sF01 <lucas.lima.1527133@sga.pucminas.br>
2024-05-19 11:53:22 -0300

    edições no JS e HTML de cadastro

---
+++ src/JS/cadastro.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#Form")
+const nameInput = document.querySelector("#Nome")
+const emailInput = document.querySelector("#Email")
+const passwordInput = document.querySelector("#Password")
+
+console.log(form,  nameInput , emailInput , passwordInput)
