82dfb78cb400ce17448e3ac3e1ae710a2cbf5334
Luc4sF01 <lucas.lima.1527133@sga.pucminas.br>
2024-05-19 12:01:01 -0300

    edições no JS

--- src/JS/cadastro.js
+++ src/JS/cadastro.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#Form")
 const nameInput = document.querySelector("#Nome")
 const emailInput = document.querySelector("#Email")
-const passwordInput = document.querySelector("#Password")
+const passwordInput = document.querySelector("#senha")
 
-console.log(form,  nameInput , emailInput , passwordInput)
+console.log(form,  nameInput , emailInput , passwordInput )
