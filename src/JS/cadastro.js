bc4cd950b0b062d6fec82dd3e88d3fc030eb20cf
Luc4sF01 <lucas.lima.1527133@sga.pucminas.br>
2024-05-21 07:10:55 -0300

    edições no JS

--- src/JS/cadastro.js
+++ src/JS/cadastro.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#Form")
 const nameInput = document.querySelector("#Nome")
 const emailInput = document.querySelector("#Email")
 const passwordInput = document.querySelector("#senha")
-
+   
 form.addEventListener("submit" ,  (event) => {
 
     event.preventDefault();
@@ -12,16 +12,34 @@ form.addEventListener("submit" ,  (event) => {
         alert("Por favor , preencha o seu nome.");
         return;
     }
+
 // Verifica se o email está vazio
-if(emailInput.value === ""){
-    alert("Por favor , preencha o seu email.");
+if(emailInput.value === "" || !isEmailValid(emailInput.value)) {
+    alert("Por favor , verifique seu e-mail.");
     return;
-
-    // Se todos os campos  estiverem corretamente preenchidos , envie o form
-    form.submit();
-
 }});
 
-function cadastrar(){
-    alert("Cadastrado")
+function isEmailValid(email){
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.test(email)) {
+        return true;
+    }
+
+    return false;
 }
+
+function validatePassword(password, minDigits){
+    if(password.length >= minDigits){
+        return true;
+    }
+    return false;
+}
+if(!validatePassword(passwordInput.value, 8)){
+    alert("A senha deve ter no mínimo 8 digitos!"); 
+    ;
+}
+
+
