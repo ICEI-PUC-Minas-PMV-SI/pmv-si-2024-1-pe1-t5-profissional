45a6b4c31baf5d12438b5d0eca8262df711c25ba
Joao Victor Farchi <jv.farchi@hotmail.com>
2024-05-27 21:20:19 -0300

    Finalizando autenticação de login

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,17 +1,49 @@
-let btn = document.querySelector(".fa-eye")
+let btn = document.querySelector('.fa-eye')
 
-btn.addEventListener("click", ()=> {
+btn.addEventListener('click', ()=> {
 
-    let inputSenha = document.querySelector("#senha");
+    let inputSenha = document.querySelector('#senha')
 
-    if (inputSenha.getAttribute("type") == "password") {
-        inputSenha.setAttribute("type", "text");
+    if (inputSenha.getAttribute('type') == 'password') {
+        inputSenha.setAttribute('type', 'text')
     } else {
-        inputSenha.setAttribute("type", "password");
+        inputSenha.setAttribute('type', 'password')
     }
 })
 
 function entrar() {
-    alert("teste");
+    let email = document.querySelector('#email')
+    let userLabel = document.querySelector('#userLabel')
 
+    let senha = document.querySelector('#senha')
+    let senhaLabel = document.querySelector('#senhaLabel')
+
+    let msgError = document.querySelector('#msgError')
+    let listaUser = []
+
+    let userValid = {
+        name: '',
+        email: '',
+        password: ''
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('users')) || []
+    
+    listaUser.forEach((item) => {
+        if (email.value == item.email && senha.value == item.password) {
+            
+            userValid = {
+                name: item.name,
+                email: item.email,
+                password: item.password
+            }
+
+        }
+    })
+
+    if (email.value == userValid.email && senha.value == userValid.password) {
+        alert ('Login efetuado com sucesso!')
+    } else {
+        alert ('Usuário ou Senha incorretos.')
+    }
 }
